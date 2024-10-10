eb76746767bd4c2f1c5c27bf11cd05fdf75dfc60
shiro-kreishi <vadas25@yandex.ru>
2024-10-10 21:04:08 +0500

    init project; add config

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"url-shortnener/internal/config"
+)
+
+func main() {
+	// TODO: init config: cleanenv
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+	// TODO: init logger: slog
+
+	// TODO init storage: sqlite
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server
+}
